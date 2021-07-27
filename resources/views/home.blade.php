f048cd087c14c31ae3543e7d7c9ec0d3dd2b181f
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-07-27 14:33:57 +0630

    carousel and data fetch in home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,18 @@
 @section('home')
     @section('content')
     {{-- {{$products}} --}}
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleControls" class="carousel slide p-5 cs" data-bs-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
+            @foreach ($products as $product)
+                <div class="carousel-item {{$product["id"]==1?'active':''}}">
+                     <img src="{{$product->gallery}}" class="d-block " alt="..." style="height:330px;margin-left:200px;width:300px;">
+                     <div class="carousel-caption d-none d-md-block" style="background-color: #2c3e50;opacity:.8;color:#2ecc71">
+                        <h5>{{strtoupper($product["name"])}}</h5>
+                        <p style="letter-spacing: 1px">{{$product["description"]}}</p>
+                    </div>
+                 </div>
+               
+            @endforeach
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
